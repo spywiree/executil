d6bc349c75c2740fb89c378166d4c06c0a6f7bcf
Spywire <antoniszymanski115@gmail.com>
2024-03-20 11:23:16 +0100

    Initial commit

---
+++ executil.go
@@ -0,0 +1,47 @@
+package executil
+
+import (
+	"errors"
+	"io/fs"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/google/shlex"
+)
+
+func IsFileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if errors.Is(err, fs.ErrNotExist) {
+		return false
+	}
+	return !info.IsDir()
+}
+
+func Command(cmd string) (*exec.Cmd, error) {
+	args, err := shlex.Split(cmd)
+	if err != nil {
+		return nil, err
+	}
+
+	path, err := filepath.Abs(args[0])
+	if err != nil {
+		return nil, err
+	}
+
+	if !IsFileExists(path) {
+		path = args[0]
+	}
+
+	return exec.Command(path, args[1:]...), nil
+}
+
+func RedirectIO(cmd *exec.Cmd) {
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+}
+
+func SetWdToParent(cmd *exec.Cmd) {
+	cmd.Dir = filepath.Dir(cmd.Path)
+}
